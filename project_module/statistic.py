c49e74e402f8f3f7f9643be3645446270c2e9730
Kim Jinung <54028026+jinyes-kim@users.noreply.github.com>
2020-05-27 00:24:37 +0900

    Update statistic.py

--- project_module/statistic.py
+++ project_module/statistic.py
@@ -1,18 +1,14 @@
 try:
+    import pandas as pd
+except ImportError as err:
+    print("라이브러리를 설치해주세요. {}".format(err))
+
+try:
     import matplotlib.pyplot as plt
 except ImportError as err:
     print("라이브러리를 설치해주세요. {}".format(err))
 
 
-"""
-1. 각각 속성별 통계 - 남녀 비율, 이탈 비율, 평균 수입, 평균 성과
-2. 부서별 통계 - 부서별 인원, 부서별 평균 임금, 부서별 평균 성과
-3. 성과 상위 30 명 - 레이팅 줄 세우기
-4. 이탈 위험 직원 -
-"""
-
-
-# functions for data-structure
 def dict_to_category(data_dict):
     res = []
     for a in data_dict:
@@ -21,66 +17,87 @@ def dict_to_category(data_dict):
     return res
 
 
-def dict_to_list(data_dict):
-    res = []
+# 딕셔너리 키들의 분포 비율 계산
+def cal_ratio(data_dict):
+    ratio = []
+    sum = 0
     for a in data_dict:
-        res.append(data_dict[a])
-    return res
+        sum += data_dict[a]
+    for a in data_dict:
+        ratio.append(data_dict[a] / sum)
+    return ratio
 
 
-def sum_dict(data_dict, data):
-    if data in data_dict:
-        data_dict[data] += 1
-    else:
-        data_dict[data] = 0
+def print_dict(data_dict):
+    for a in data_dict:
+        print("{}: {}".format(a, data_dict[a]))
 
 
-# functions for statistics
-def statistic(data, part=False):
-    # data is employee instance
-
-    # Age, gender, Attrition, department, jobRole, married, income, rate
-    sum_age = 0
-    gender_dict = {}
-    att_dict = {}
-    depart_dict = {}
-    role_dict = {}
-    marry_dict = {}
-    sum_income = 0
-    sum_rate = 0
-
-    if not part:
-        for a in data:
-            sum_age += int(a.age)
-            sum_dict(gender_dict, a.gender)
-            sum_dict(att_dict, a.attrition)
-            sum_dict(depart_dict, a.department)
-            sum_dict(role_dict, a.job_role)
-            sum_dict(marry_dict, a.marital_status)
-            sum_income += int(a.income)
-            sum_rate += int(a.rate)
+# 데이터셋이 리스트 안에 객체로 존재하는 상태.
+def make_df(data):
+    tmp = []
+    label = ['Age',	'Attrition', 'Department', 'EmployeeNumber', 'Gender', 'JobLevel', 'JobRole',
+             'MaritalStatus', 'MonthlyIncome', 'MonthlyRate', 'OverTime']
+    for a in data:
+        tmp.append(a.total)
+    df = pd.DataFrame(tmp, columns=label)
+    return df
 
 
-    else:
-        # 부서별 통계
-        pass
+def list_to_dict(data_list):
+    data_dict = {}
+    for a in data_list:
+        if a in data_dict:
+            data_dict[a] += 1
+        else:
+            data_dict[a] = 0
+    return data_dict
 
-    sum_age /= len(data)
-    sum_income /= len(data)
-    sum_rate /= len(data)
 
-    print("평균 나이: ", sum_age)
-    print("성별 분포: ", gender_dict)
-    print("이탈 비율: ", att_dict)
-    print("부서 분포: ", depart_dict)
-    print("직무 분포: ", role_dict)
-    print("결혼 비율: ", marry_dict)
-    print("평균 급여: ", sum_income)
-    print("평균 성과: ", sum_rate)
-
-    gender_data = dict_to_list(gender_dict)
-    gender_category = dict_to_category(gender_dict)
-    plt.pie(gender_data, labels=gender_category, autopct="%.1f%%")
-
-    # 위 코드 기반으로 서브플롯 그려서 각 항몽에 대한 파이 파트 출력
+def cate_graph(data, kwd):
+    df = make_df(data)
+    tmp = df[kwd]
+    data_dict = list_to_dict(tmp)
+    print_dict(data_dict)
+    col = dict_to_category(data_dict)
+    plt.pie(cal_ratio(data_dict), labels=col, autopct="%.1f%%")
+    plt.title(kwd)
     plt.show()
+
+
+def numeric_graph(data, kwd):
+    df = make_df(data)
+    tmp = df[kwd]
+    tmp = list(map(int, tmp))
+    avg = sum(tmp) / len(tmp)
+    print("평균 {}: {:.2f}".format(kwd, avg))
+
+    binn = 0
+    if kwd == 'Age':
+        binn = 10
+    else:
+        binn = 100
+    plt.hist(tmp, label=kwd, bins=binn, )
+    plt.title(kwd)
+    plt.tick_params(axis='both', which='both', direction='in', pad=8, top=True, right=True)
+    plt.show()
+
+
+def summary_data(data):
+    # 출력 모양 이쁘게 변환, 평균 사분위수 계산
+    df = make_df(data)
+    for kwd in df.columns:
+        if kwd == 'EmployeeNumber':
+            continue
+        if kwd == "Age" or kwd == 'MonthlyIncome' or kwd == 'MonthlyRate':
+            tmp = df[kwd]
+            tmp = list(map(int, tmp))
+            avg = sum(tmp) / len(tmp)
+            print("[요약: {}]".format(kwd))
+            print("{}: {:.2f}\n\n".format(kwd, avg))
+        else:
+            tmp = df[kwd]
+            data_dict = list_to_dict(tmp)
+            print("[요약: {}]".format(kwd))
+            print_dict(data_dict)
+            print()
