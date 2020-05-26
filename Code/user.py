58e1334a159d792f5705a43c65f4afc65ceed885
Kim Jinung <54028026+jinyes-kim@users.noreply.github.com>
2020-05-26 18:46:23 +0900

    Update user.py

--- Code/user.py
+++ Code/user.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 class User:
     def __init__(self, id, power):
         self.id = id
@@ -6,4 +5,4 @@ class User:
 
     def print_identity(self):
         level = ['일반 사용자', '관리자']
-        print("성명: {}\t직급: {}".format(self.id, level[self.power]))
+        print(" 성명: {}\t권한: {}".format(self.id, level[self.power]))
