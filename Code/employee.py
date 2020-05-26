d21f70f8d6e1fde46c872b5fcdbb7027e2a2e00b
Kim Jinung <54028026+jinyes-kim@users.noreply.github.com>
2020-05-27 00:24:16 +0900

    Update employee.py

--- Code/employee.py
+++ Code/employee.py
@@ -4,6 +4,7 @@ class Employee:
         #  JobRole	MaritalStatus	MonthlyIncome	MonthlyRate	 OverTime
 
         self.total = line.split(',')
+
         self.age = self.total[0]
         self.attrition = self.total[1]
         self.department = self.total[2]
@@ -15,6 +16,7 @@ class Employee:
         self.income = self.total[8]
         self.rate = self.total[9]
         self.overTime = self.total[10]
+        self.overTime = ''.join(ch.lower() for ch in self.total[10] if ch.isalnum())
 
     def print_HR(self, admin):
         if admin == 1:
