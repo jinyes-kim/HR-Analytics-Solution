7217d570b3ec4bfd27c421c2a2e7864f3770f124
Kim Jinung <54028026+jinyes-kim@users.noreply.github.com>
2020-05-26 17:45:22 +0900

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,71 +1,171 @@
-import datetime
+# -*- coding: utf-8 -*-
+
+import time
+import user
+import employee
+from custom_module import search
+from custom_module import statistic
+from custom_module import view
 from custom_module import login
 from custom_module import load
 
-# DB & user data load
-login.view_load()
+
+# DB & data load
 try:
     db = login.connect_db()
-
 except Exception as err:
     print("ERROR: {}".format(err))
 
 try:
     data = load.load_file()
-except:
+except Exception as err:
     print("ERROR: {}".format(err))
 
-login.view_init()
+
+# login effect
+view.load()
+time.sleep(1)
+view.clear()
+view.init()
 buffer = input()
 
 # main
 ban = 0
 while True:
     success = False
-    login.clear_screen()
-    login.view_start()
+    view.clear()
+    view.start()
+
+    # login
     id_input = input('ID: ')
     pw_input = input('PW: ')
-    try:
-        if login.login(db, id_input, pw_input):
-            user = login.User(id_input, db[id_input][2])
-            login.clear_screen()
-            login.view_login(id_input)
-            print("\n{}님 환영합니다.\n접속 시각: {}".format(user.id, datetime.datetime.now()))
-            success = True
-        else:
-            raise KeyError
-    except:
-        print("\nID가 존재하지 않거나 PASSWORD가 다릅니다.")
-        print("다시 로그인 하시려면 ENTER를 눌러주세요")
+    if login.login(db, id_input, pw_input):
+        success = True
+    else:
+        view.login_fail()
         buffer = input()
-        login.clear_screen()
+        view.clear()
         ban += 1
         if ban == 3:
             print("잘못된 접근으로 인한 IP 차단")
             break
 
+    # start menu
     if success:
+        identity = user.User(id_input, db[id_input][2]) # 유저 객체로 권한 부여
+        view.clear()
+        view.login_success(id_input)
         while True:
-            print("======Menu=====\n(1) 조회\n(2) 조직도\n(3) 인사 관리\n(0) 종료")
+            identity.print_identity()
+            view.menu_main(identity.power)
             try:
                 n = int(input("\n=> "))
             except:
-                print("실행할 수 없는 명령어입니다.")
+                print("잘못된 명령어입니다.")
             if n == 0:
                 break
+
+            # 조직도
             elif n == 1:
-                login.clear_screen()
-                print("======Menu=====\n(1) 전체 출력\n(2) 키워드 출력\n(0) 뒤로가기")
+                view.clear()
                 while True:
-                    n = int(input("\n=>"))
-                    if n == 1:
+                    view.menu_organize()
+                    try:
+                        n = int(input("\n=> "))
+                    except:
+                        print("잘못된 명령어입니다.")
+                        continue
+                    if n == 0:
+                        view.clear()
+                        break
+                    elif n == 1:
+                        print("Department: Human Resources")
                         for a in data:
-                            a.print_HR()
-                        break
-                    elif n == 0:
-                        break
+                            if search.search_organize(a, 'Human Resources'):
+                                a.print_HR(identity.power)
+                    elif n == 2:
+                        print("Department: Research & Development")
+                        for a in data:
+                            if search.search_organize(a, 'Research & Development'):
+                                a.print_HR(identity.power)
+                    elif n == 3:
+                        print("Department: Sales")
+                        for a in data:
+                            if search.search_organize(a, 'Sales'):
+                                a.print_HR(identity.power)
                     else:
                         print("잘못된 명령어입니다.")
+                        continue
 
+            # 직원 조회
+            elif n == 2:
+                view.clear()
+                while True:
+                    view.menu_search_employee()
+                    try:
+                        print("메뉴를 입력하세요.")
+                        n = int(input("\n=> "))
+                    except:
+                        print("잘못된 명령어입니다.")
+                        continue
 
+                    if n == 0:
+                        break
+                    elif n == 1:
+                        employee.print_column(identity.power)
+                        for a in data:
+                            a.print_HR(identity.power)
+                    elif n == 2:
+                        print("검색할 키워드를 입력해주세요.")
+                        kwd = input("\n=>")
+                        for a in data:
+                            if search.search_keyword(a, kwd):
+                                a.print_HR(identity.power)
+                    else:
+                        print("잘못된 명령어입니다.")
+                        continue
+
+            # 직원 통계
+            elif n == 3:
+                while True:
+                    if identity.power == 1:
+                        view.clear()
+                        view.menu_statistic()
+                        try:
+                            n = int(input("\n=> "))
+                        except:
+                            print("잘못된 명령어입니다.")
+                        if n == 0:
+                            break
+                        elif n == 1:
+                            print("전체 요약 통계")
+                            statistic.statistic(data)
+                            pass
+                        else:
+                            print("잘못된 명령어입니다.")
+                    else:
+                        print("잘못된 명령어입니다.")
+                        break
+
+            # 인사 관리
+            elif n == 4:
+                while True:
+                    if identity.power == 1:
+                        view.clear()
+                        view.menu_management()
+                        try:
+                            n = int(input("\n=> "))
+                        except:
+                            print("잘못된 명령어입니다.")
+                        if n == 0:
+                            break
+                        elif n == 1:
+                            pass
+                        else:
+                            print("잘못된 명령어입니다.")
+                    else:
+                        print("잘못된 명령어입니다.")
+                        break
+
+            else:
+                print("잘못된 명령어입니다.")
