ff44ab67d689cfbe9d7f01456056511837c46efa
Kim Jinung <54028026+jinyes-kim@users.noreply.github.com>
2020-05-26 18:48:14 +0900

    Update search.py

--- project_module/search.py
+++ project_module/search.py
@@ -1,3 +1,6 @@
+# 추가 기능 - 다중 검색 기능
+
+# 해당하는 부서에 해당하면 필터링
 def search_organize(employee, keyword):
     if keyword == employee.department:
         return True
@@ -5,6 +8,7 @@ def search_organize(employee, keyword):
         return False
 
 
+# 키워드에 해당하는 직원 필터링
 def search_keyword(employee, keyword):
     temp = employee.total
     if keyword.isalpha():
@@ -28,3 +32,4 @@ def search_keyword(employee, keyword):
             elif keyword - 1000 < int(employee.rate) < keyword + 1000:
                 return True
     return False
+
