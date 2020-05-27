b166a7f1ef3e8e14d2112692f84dd05d1508aaf7
Kim Jinung <54028026+jinyes-kim@users.noreply.github.com>
2020-05-27 21:08:41 +0900

    Update load.py

--- project_module/load.py
+++ project_module/load.py
@@ -1,11 +1,19 @@
 import employee
 
 
-def load_file():
+def load_file(att=False, not_att=False):
     data = []
     with open('./data/data.csv', 'r') as file:
         for idx, line in enumerate(file):
+            tmp = line.split(',')
             if idx == 0:
                 continue
-            data.append(employee.Employee(line))
+            if att:
+                if tmp[1] == 'Yes':
+                    data.append(employee.Employee(line))
+            elif not_att:
+                if tmp[1] == 'No':
+                    data.append(employee.Employee(line))
+            else:
+                data.append(employee.Employee(line))
     return data
